d7542ee0aa4b389362ee406c54b21284865accb4
Maurício Moreira Neto <maumneto@gmail.com>
2018-10-23 16:54:04 -0300

    first commit

---
+++ pilhaEstatica/pilha.py
@@ -0,0 +1,11 @@
+class pilhaEstatica:
+    
+    def __init__(self):
+        self.pilha = []
+    
+    def inserir_elemento(self, dado):
+        return self.pilha.append(dado)
+    
+    def remover_elemento(self):
+        self.pilha.pop()
+    
